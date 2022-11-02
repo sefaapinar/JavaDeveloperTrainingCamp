7a73e858f0b61e72f0b90af078275bdc6cc8ab37
sefaapinar <sefanur.pinar@gmail.com>
2022-11-02 23:03:40 +0300

    Response - Request

--- CampSpring/rentACar/src/main/java/kodlama/io/rentACar/webApi/controllers/BrandsController.java
+++ CampSpring/rentACar/src/main/java/kodlama/io/rentACar/webApi/controllers/BrandsController.java
@@ -2,13 +2,14 @@ package kodlama.io.rentACar.webApi.controllers;
 
 import java.util.List;
 
-
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import kodlama.io.rentACar.business.abstracts.BrandService;
-import kodlama.io.rentACar.entities.concretes.Brand;
+import kodlama.io.rentACar.business.requests.CreateBrandRequest;
+import kodlama.io.rentACar.business.responses.GetAllBrandsResponse;
 
 @RestController  //annotation = bilgilendirme ifadesidir. Bu uygulamayı derlediğinde veya çalışırken kullanır.
 @RequestMapping("/api/brands")  // Adresimizin sonuna brands api derse bu kontrolü çalıştırır.
@@ -25,9 +26,13 @@ public class BrandsController {
 	}
 	
 	@GetMapping("/getAll")
-	public List<Brand> getAll(){
+	public List<GetAllBrandsResponse> getAll(){
 		//IoC Default gelir springbootta.
 		return brandService.getAll();
 	}
 	
+	@PostMapping("/add")
+	public void add(CreateBrandRequest createBrandRequest) {
+		this.brandService.add(createBrandRequest);
+	}
 }
